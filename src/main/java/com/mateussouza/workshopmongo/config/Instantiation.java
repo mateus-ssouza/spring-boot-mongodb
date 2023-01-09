3a53a5acdfe70759c03b5f17dc3097addb621dce
Mateus Souza <msouzacrv@gmail.com>
2023-01-09 10:10:26 -0300

    Referencing user posts

--- src/main/java/com/mateussouza/workshopmongo/config/Instantiation.java
+++ src/main/java/com/mateussouza/workshopmongo/config/Instantiation.java
@@ -38,5 +38,8 @@ public class Instantiation implements CommandLineRunner {
         Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
         Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
         postRepository.saveAll(Arrays.asList(p1, p2));
+
+        u1.getPosts().addAll(Arrays.asList(p1, p2));
+        userRepository.save(u1);
     }
 }
